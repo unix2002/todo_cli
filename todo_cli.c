db738424fc42438fb3d62b5596693271bec46b9b
Joseph <josephkrol2002@gmail.com>
2022-10-23 17:05:08 +0200

    todo_cli.c toegevoegd

---
+++ todo_cli.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+    Todo_cli.c is een programma dat bedoelt is om dagelijks een lijst bij te houden met taken.
+    todo_cli genereert een score gebasseerd op het aantal geslaagde taken.
+    Het maakt gebruik van invoer.
+*/
+int main(void) {
+    printf("tweede git commit.");
+    return 0;
+}
