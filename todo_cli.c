a1bec1478fb432781c607136fbc74100992afa6b
Joseph <josephkrol2002@gmail.com>
2022-10-24 14:47:23 +0200

    union en struct toegevoegd voor het organiseren van de takendata

--- todo_cli.c
+++ todo_cli.c
@@ -8,41 +8,64 @@
     Het maakt gebruik van invoer.
 */
 
+
+union taak_num {
+    int datum;
+    int score;
+};
+
+struct taak_item {
+    char *taak_zin;
+    union taak_num taaknum;
+};
+
+typedef struct taak_item taak_item;
+
+
+
 /*Deze functie vraagt de gebruiker om invoer.
  * */
-void vraag_lijst(void) {
+char *vraag_lijst(void) {
     char toets;
-    char taak[300];
-    for (int i = 0; i < (int)sizeof(taak); i++) {
+    char *taak = malloc(300*sizeof(int));
+    
+    for (int i = 0; i < (int)(300*sizeof(int)); i++) {
         taak[i] = 0;
     }
+
     int i = 0;
 
     printw("Noem een taak:\n");
     while (toets != '\n') {
         toets = getch();
-        if (toets != (char)KEY_BACKSPACE || toets != '\n') {
+        if (toets != 'k' || toets != '\n') {
             taak[i] = toets;
             printw("%c", toets);
         }
-        else if (toets == (char)KEY_BACKSPACE && i != 0) {
-            i--;
-            printw("jajas");
-            taak[i] = '0';
-            clear();
-            for (int j = 0; j < (int)sizeof(taak); j++) {
-                printw("%c", taak[j]);
-                if (taak[j] == '0'){
-                    break;
-                }
-            }
-        }
-        
         i++;
     }
-    clear();
-    printw("klaar");
-    getch();
+
+    return taak;
+}
+
+taak_item *maak_taak_struct(char *taak, int datum, int score) {
+    static taak_item task;    
+    task.taaknum.datum = datum;
+    task.taaknum.score = score;
+    task.taak_zin = taak;
+
+    return &task;
+}
+
+/*Deze functie print de inhoud van een array
+*/
+void print_array(char ar[]) {
+    //for (int i = 0; i < (int)(TAAK_GROOTTE*sizeof(int)); i++) {
+    //printw("%c%c%c", ar[0], ar[1], ar[2]);
+    for (int i = 0; i < (int)(300*sizeof(int)); i++) {
+        printw("%c", ar[i]);
+    }
+            
 }
 
 int main(void) {
@@ -50,7 +73,18 @@ int main(void) {
     noecho();
     clear();
     keypad(stdscr, true);
-    vraag_lijst(); 
+
+    char *taak = vraag_lijst(); 
+    print_array(taak);
+    
+    int datum = 20;
+    //int datum = datum_vraag();
+    int score = 30;
+    //int score = score_vraag();
+    maak_taak_struct(taak, datum, score);
+
+    getch();
     endwin();
+    free(taak);
     return 0;
 }
