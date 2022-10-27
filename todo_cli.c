3db5a69b1decff99ca2df5ea2ddae067c0d7c755
Joseph <josephkrol2002@gmail.com>
2022-10-27 17:49:36 +0200

    De build wordt te rommelig. Ik ga proberen alles in een .dat file op te slaan.

--- todo_cli.c
+++ todo_cli.c
@@ -5,11 +5,14 @@
 #include <assert.h>
 #include <time.h>
 #include <string.h>
+//#include "ui.c"
 /*
     Todo_cli.c is een programma dat bedoelt is om dagelijks een lijst bij te houden met taken.
     todo_cli genereert een score gebasseerd op het aantal geslaagde taken.
     Het maakt gebruik van invoer.
 */
+
+int AANTAL_TAKEN = 4;
 
 // Union zorgt dat de twee variabelen hetzelfde geheugenadress delen.
 struct taak_num {
@@ -27,31 +30,44 @@ typedef struct taak_item taak_item;
 /*
 Deze functie vraagt de gebruiker om invoer.
 */
-char *vraag_lijst(void) {
+char **vraag_lijst(void) {
     int toets;
-    size_t grootte = 300*sizeof(int);    
-    char *taak = malloc(grootte);
+    size_t grootte = 300*sizeof(int);
+    char **taken_str_lijst = malloc(AANTAL_TAKEN*sizeof(int)*300);
 
-    for (int i = 0; i < (int)grootte; i++) {
-        taak[i] = 0;
-    }
-        
-    int i = 0;
-    printw("Noem een taak:\n");
-    while (toets != '\n') {
-        toets = getch();
-        if ((int)toets == 263) {
-            if (taak[i] != 0) {
-                taak[i] = 0;
-                break;
-            }
+    for (int j = 0; j < AANTAL_TAKEN; j++) {      
+       taken_str_lijst[j] = malloc(grootte);
+        for (int i = 0; i < (int)grootte; i++) {
+            taken_str_lijst[j][i] = 0;
         }
 
-        taak[i] = toets;
-        printw("%c", toets);
-        i++;
+        int a = 0;
+        while (toets != '\n') {
+            printw("Noem een taak:\n");
+            toets = getch();
+            if ((int)toets == KEY_BACKSPACE) {
+                if (taken_str_lijst[j][a] != 0) {
+                    taken_str_lijst[j][a] = 0;
+                    delch();
+                    break;
+                }
+            }
+            taken_str_lijst[j][a] = toets;
+            printw("%c", toets);
+            a++;
+        }
     }
-    return taak;
+    return taken_str_lijst;
+}
+
+/*
+Deze functie bevrijd het geheugen dat gebruikt wordt in de maak_lijst array.
+Daarmee doel ik specifiek op de "taken_str_lijst" array.
+*/
+void free_lijst(char **taken) {
+    for (int i = 0; i < AANTAL_TAKEN; i++) {
+        free(taken[i]);
+    }
 }
 
 /*
@@ -87,6 +103,7 @@ int score_vraag(void) {
     }
     if (valide_invoer == 0) {
         printw("error, invalide invoer");
+        endwin();
         getch();
         exit(1);
     }
@@ -104,14 +121,14 @@ void init_ncurses(void) {
 /*
 Deze functie organiseert de taken als enkele speciale variabele,
 */
-taak_item *maak_taak(char *datum_str, char *taak_zin){
+taak_item *maak_taak(char *datum_str, char *taak_zin) {
     static taak_item task;
     time_t d;
     time(&d);
     task.taak_zin = taak_zin;
     task.taaknum.datum = ctime(&d);
     task.taaknum.score = score_vraag()-48;
-    return &task; 
+    return &task;
 }
 
 /*Print alle info in de taak op een overzichtelijke manier.*/
@@ -124,17 +141,38 @@ void print_taak(taak_item *taak) {
     getch();
 }
 
-int main(void) {
-    init_ncurses();
-    char *datum_str = malloc(sizeof(time_t));
-    char *taak_zin = vraag_lijst();
- 
-    taak_item *taak_een = maak_taak(datum_str, taak_zin);
+taak_item *maak_item_array(void) {
 
-    print_taak(taak_een);
+    taak_item *taken_array = malloc(10*sizeof(taak_item));
+    char **taken_str_lijst = vraag_lijst();
+
+    for (int i = 0; i < AANTAL_TAKEN; i++) {
+        taak_item *taak = maak_taak(malloc(sizeof(time_t)), taken_str_lijst[i]);
+        taken_array[i] = *taak;
+    }
+    free(taken_str_lijst);
+    return taken_array;
+}
+
+void free_item_array(taak_item* taken_array) {
+    
+    for(int i = 0; i < AANTAL_TAKEN; i++) {
+        free(taken_array[i].taaknum.datum);
+    }
+    free(taken_array[AANTAL_TAKEN].taak_zin);
+}
+
+int main(void) {
+    
+    init_ncurses();
+    taak_item *taken_item = maak_item_array();
+    
+    for(int i; i < AANTAL_TAKEN; i++) {
+        free(taken_item[i].taak_zin);
+        free(taken_item[i].taaknum.datum);
+    }
+    free(taken_item);
 
     endwin();
-    free(datum_str);
-    free(taak_zin);
     return 0;
 }
---
+++ ui.c
@@ -0,0 +1,3 @@
+int return_1(void) {
+    return 1;
+}
